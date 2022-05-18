efd54cc5d09ef020f32b51e3cb0f3015ad9aa09d
Álvaro Real <105717687+loeggft@users.noreply.github.com>
2022-05-18 10:09:05 +0200

    Created Melon.java with constructor, getters and toString. Updated Main.java

--- src/main/java/com/sinensia/lambdas/Main.java
+++ src/main/java/com/sinensia/lambdas/Main.java
@@ -1,5 +1,8 @@
 package com.sinensia.lambdas;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/sinensia/lambdas/Melon.java
@@ -0,0 +1,34 @@
+package com.sinensia.lambdas;
+
+public class Melon {
+    private final String type;
+    private final int weight;
+    private final String origin;
+
+    public Melon(
+            String type,
+            int weight,
+            String origin
+    ) {
+        this.type = type;
+        this.weight = weight;
+        this.origin = origin;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    @Override
+    public String toString() {
+        return "El melon es un " + type + " que pesa " + weight + ", y viene de " + origin;
+    }
+}
